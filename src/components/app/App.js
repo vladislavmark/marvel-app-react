373f9c9758599bd0de8d3dd45f9a89f920f8defd
mark <vladys.marchenko@gmail.com>
2023-09-06 13:12:50 +0300

    155 homework(PropTypes)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
@@ -98,6 +99,10 @@ class CharList extends Component {
             </div>
         )
     }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired,
 }
 
 export default CharList;
